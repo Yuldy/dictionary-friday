4cf2ea52cc28ca85013b9f932faad8ac83f75bd1
Christian Phan <chrisvup@gmail.com>
2024-03-15 20:17:56 -0700

    Got it working, should do some more extensive testing in regards to where I want to furhter build with this

--- main.py
+++ main.py
@@ -28,8 +28,36 @@ async def send_message(message: Message, user_message: str) -> None:
     try:
         response: str = get_response(user_message)
         # If channel is private, send it to author, or send it back to current channel
-        await message.author.send(response) if is_private else message.channel.send(response)
+        await message.author.send(response) if is_private else await message.channel.send(response)
     
     # We can change the exception later
     except Exception as e:
         print("An error has occured as ", e)
+
+
+# Handling startup of bot
+@client.event
+async def on_ready() -> None:
+    print(f'{client.user} is now running!')
+
+# Handling Incoming Messages
+@client.event
+async def on_message(message: Message) -> None:
+    # Bot is the one who wrote the message
+    if message.author == client.user:
+        return
+    
+    username: str = str(message.author)
+    user_message: str = message.content
+    channel: str = str(message.channel)
+
+    print(f'[{channel} {username}: "{user_message}"]')
+    await send_message(message, user_message)
+
+# Main entry point
+
+def main() -> None:
+    client.run(token=TOKEN)
+
+if __name__ == '__main__':
+    main()
