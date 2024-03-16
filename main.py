0da4d9c18135cddb6544249d20a844e59ed13dbb
Christian Phan <chrisvup@gmail.com>
2024-03-15 20:08:10 -0700

    Working on the responses for the discord bot

--- main.py
+++ main.py
@@ -7,4 +7,29 @@ from responses import get_response
 # Load Token:
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
-print(TOKEN)
+
+# Bot Setup
+intents: Intents = Intents.default()
+intents.message_content = True # NOQA
+client: Client = Client(intents=intents)
+
+# Message Functionality
+async def send_message(message: Message, user_message: str) -> None:
+    if not user_message:
+        print('(Message was empty because intents were not enabled probably)')
+        return
+    
+    # Users can message privately
+    # Question marks is message private
+    is_private = user_message[0] == '?'
+    if is_private:
+        user_message = user_message[1:]
+
+    try:
+        response: str = get_response(user_message)
+        # If channel is private, send it to author, or send it back to current channel
+        await message.author.send(response) if is_private else message.channel.send(response)
+    
+    # We can change the exception later
+    except Exception as e:
+        print("An error has occured as ", e)
--- responses.py
+++ responses.py
@@ -1,2 +1,16 @@
+from random import choice, randint
+
+
 def get_response(user_input: str) -> str:
-    raise NotImplementedError("Code is missing.")
+    lowered: str = user_input.lower()
+
+    # Replace logic later and response algorithm
+    if lowered == '':
+        return "Well you are awfully silent.."
+    elif 'hello' in lowered:
+        return "Hello there!"
+    elif 'bye' in lowered:
+        return "See you!"
+    
+    else:
+        return choice(["I do not understand.", "What did you say?"])
